5e338a00cc1c2b9d67fdbfbc7fc13ca761d27565
Peter1980FFM <45266139+Peter1980FFM@users.noreply.github.com>
2026-01-18 00:12:29 +0100

    Create axp2101_sensor.cpp

---
+++ components/axp2101/axp2101_sensor.cpp
@@ -0,0 +1,10 @@
+#include "axp2101_sensor.h"
+#include "axp2101.h"
+
+namespace esphome {
+namespace axp2101 {
+
+// All logic is handled in AXP2101Component::update()
+
+}  // namespace axp2101
+}  // namespace esphome
