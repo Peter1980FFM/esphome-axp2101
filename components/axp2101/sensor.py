2de138ed89d2cd7dc35b22f5453b1732b678d18a
Peter1980FFM <45266139+Peter1980FFM@users.noreply.github.com>
2026-01-18 00:38:45 +0100

    Update sensor.py

--- components/axp2101/sensor.py
+++ components/axp2101/sensor.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, i2c
-from esphome.components import text_sensor
+from esphome.components import sensor, i2c, text_sensor
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["i2c"]
