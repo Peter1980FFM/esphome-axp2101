fc05322a251ffc9ae0b04df5b022ac3c600b3bed
Peter1980FFM <45266139+Peter1980FFM@users.noreply.github.com>
2026-01-17 22:14:45 +0100

    Update axp2101.h

--- components/axp2101/axp2101.h
+++ components/axp2101/axp2101.h
@@ -6,9 +6,6 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/core/component.h"
 
-#define XPOWERS_CHIP_AXP2101
-#include "XPowersLib.h"
-
 namespace esphome {
 namespace axp2101 {
 
@@ -38,91 +35,81 @@ public:
   void set_brightness(float brightness) { brightness_ = brightness; }
   void set_model(AXP2101Model model) { this->model_ = model; }
 
-  // ========== INTERNAL METHODS ==========
-  // (In most use cases you won't need these)
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
   void update() override;
 
 private:
-    static std::string GetStartupReason();
+  static std::string GetStartupReason();
 
 protected:
-    sensor::Sensor *batteryvoltage_sensor_;
-    sensor::Sensor *batterylevel_sensor_;
-    binary_sensor::BinarySensor *batterycharging_bsensor_;
-    float brightness_{1.0f};
-    float curr_brightness_{-1.0f};
-    AXP2101Model model_;
+  sensor::Sensor *batteryvoltage_sensor_{nullptr};
+  sensor::Sensor *batterylevel_sensor_{nullptr};
+  binary_sensor::BinarySensor *batterycharging_bsensor_{nullptr};
+  float brightness_{1.0f};
+  float curr_brightness_{-1.0f};
+  AXP2101Model model_{AXP2101_M5CORE2};
 
-    /** M5Stack Core2 Values
-     * LDO2: ILI9342C PWR (Display)
-     * LD03: Vibration Motor
-     */
+  void UpdateBrightness();
+  bool GetBatState();
+  uint8_t GetBatData();
 
-    void  UpdateBrightness();
-    bool  GetBatState();
-    uint8_t  GetBatData();
+  void  EnableCoulombcounter(void);
+  void  DisableCoulombcounter(void);
+  void  StopCoulombcounter(void);
+  void  ClearCoulombcounter(void);
+  uint32_t GetCoulombchargeData(void);
+  uint32_t GetCoulombdischargeData(void);
+  float GetCoulombData(void);
 
-    void  EnableCoulombcounter(void);
-    void  DisableCoulombcounter(void);
-    void  StopCoulombcounter(void);
-    void  ClearCoulombcounter(void);
-    uint32_t GetCoulombchargeData(void);
-    uint32_t GetCoulombdischargeData(void);
-    float GetCoulombData(void);
+  uint16_t GetVbatData(void) __attribute__((deprecated));
+  uint16_t GetIchargeData(void) __attribute__((deprecated));
+  uint16_t GetIdischargeData(void) __attribute__((deprecated));
+  uint16_t GetTempData(void) __attribute__((deprecated));
+  uint32_t GetPowerbatData(void) __attribute__((deprecated));
+  uint16_t GetVinData(void) __attribute__((deprecated));
+  uint16_t GetIinData(void) __attribute__((deprecated));
+  uint16_t GetVusbinData(void) __attribute__((deprecated));
+  uint16_t GetIusbinData(void) __attribute__((deprecated));
+  uint16_t GetVapsData(void) __attribute__((deprecated));
+  uint8_t GetBtnPress(void);
 
-    uint16_t GetVbatData(void) __attribute__((deprecated));
-    uint16_t GetIchargeData(void) __attribute__((deprecated));
-    uint16_t GetIdischargeData(void) __attribute__((deprecated));
-    uint16_t GetTempData(void) __attribute__((deprecated));
-    uint32_t GetPowerbatData(void) __attribute__((deprecated));
-    uint16_t GetVinData(void) __attribute__((deprecated));
-    uint16_t GetIinData(void) __attribute__((deprecated));
-    uint16_t GetVusbinData(void) __attribute__((deprecated));
-    uint16_t GetIusbinData(void) __attribute__((deprecated));
-    uint16_t GetVapsData(void) __attribute__((deprecated));
-    uint8_t GetBtnPress(void);
+  void SetSleep(void);
+  void DeepSleep(uint64_t time_in_us = 0);
+  void LightSleep(uint64_t time_in_us = 0);
 
-      // -- sleep
-    void SetSleep(void);
-    void DeepSleep(uint64_t time_in_us = 0);
-    void LightSleep(uint64_t time_in_us = 0);
+  void SetChargeCurrent(uint8_t);
+  float GetBatCurrent();
+  float GetVinVoltage();
+  float GetVinCurrent();
+  float GetVBusVoltage();
+  float GetVBusCurrent();
+  float GetTempInAXP2101();
+  float GetBatPower();
+  float GetBatChargeCurrent();
+  float GetAPSVoltage();
+  float GetBatCoulombInput();
+  float GetBatCoulombOut();
+  uint8_t GetWarningLevel(void);
+  void SetCoulombClear();
+  void SetLDO2(bool State);
+  void SetLDO3(bool State);
+  void SetAdcState(bool State);
 
-    // void SetChargeVoltage( uint8_t );
-    void  SetChargeCurrent( uint8_t );
-    float GetBatCurrent();
-    float GetVinVoltage();
-    float GetVinCurrent();
-    float GetVBusVoltage();
-    float GetVBusCurrent();
-    float GetTempInAXP2101();
-    float GetBatPower();
-    float GetBatChargeCurrent();
-    float GetAPSVoltage();
-    float GetBatCoulombInput();
-    float GetBatCoulombOut();
-    uint8_t GetWarningLevel(void);
-    void SetCoulombClear();
-    void SetLDO2( bool State );
-    void SetLDO3( bool State );
-    void SetAdcState(bool State);
+  void PowerOff();
 
-    void PowerOff();
-
-
-    void Write1Byte( uint8_t Addr ,  uint8_t Data );
-    uint8_t Read8bit( uint8_t Addr );
-    uint16_t Read12Bit( uint8_t Addr);
-    uint16_t Read13Bit( uint8_t Addr);
-    uint16_t Read16bit( uint8_t Addr );
-    uint32_t Read24bit( uint8_t Addr );
-    uint32_t Read32bit( uint8_t Addr );
-    void ReadBuff( uint8_t Addr , uint8_t Size , uint8_t *Buff );
+  void Write1Byte(uint8_t Addr, uint8_t Data);
+  uint8_t Read8bit(uint8_t Addr);
+  uint16_t Read12Bit(uint8_t Addr);
+  uint16_t Read13Bit(uint8_t Addr);
+  uint16_t Read16bit(uint8_t Addr);
+  uint32_t Read24bit(uint8_t Addr);
+  uint32_t Read32bit(uint8_t Addr);
+  void ReadBuff(uint8_t Addr, uint8_t Size, uint8_t *Buff);
 };
 
-}
-}
+}  // namespace axp2101
+}  // namespace esphome
 
 #endif
